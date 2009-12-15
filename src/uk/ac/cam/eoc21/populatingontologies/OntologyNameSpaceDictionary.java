41f496ed267fb5ce31c89eaf3cdbeecd20ea57fb
Ed Cannon <eoc21@cam.ac.uk>
2009-12-15 16:19:39 +0000

    Created a dictionary of ontology namespaces and uri locations. Also created a class to process ontology information such as adding instances or merging multiple ontologies.

---
+++ src/uk/ac/cam/eoc21/populatingontologies/OntologyNameSpaceDictionary.java
@@ -0,0 +1,22 @@
+package populatingontologies;
+/**
+ * Class to retain information on the namespaces of the separate ontologies.
+ * @author ed
+ *
+ */
+public class OntologyNameSpaceDictionary {
+	public static final String PROPERTY_NS = "http://www.polymerinformatics.com/ChemAxiom/ChemAxiomProp.owl#";
+	public static final String PROPERTY_URI = "http://ontologies.googlecode.com/svn/trunk/src/ChemAxiomProp.owl";
+	public static final String REPEATUNIT_NS = "http://www.chemoinformatician.co.uk/RepeatUnits.owl#";
+	public static final String REPEATUNIT_URI = "http://chemoinformatician.co.uk/RepeatUnitRDF.owl";
+	public static final String MEASUREMENT_TECHNIQUES_NS = "http://www.polymerinformatics.com/ChemAxiom/ChemAxiomMetrology.owl#";
+	public static final String MEASUREMENT_TECHNIQUES_URI = "http://ontologies.googlecode.com/svn/trunk/src/ChemAxiomMetrology.owl";
+	public static final String MEASUREMENT_STANDARDS_NS = "http://www.polymerinformatics.com/DPI#";
+	public static final String MEASUREMENT_STANDARDS_URI = "http://ontologies.googlecode.com/svn/trunk/src/MeasurementStandards.owl";
+	public static final String MEASUREMENT_CONDITIONS_NS = "http://www.polymerinformatics.com/MeasurementConditionIntegrated#";
+	public static final String MEASUREMENT_CONDITIONS_URI = "http://ontologies.googlecode.com/svn/trunk/src/MeasurementConditionIntegrated.owl";
+	public static final String UNITS_NS = "http://www.polymerinformatics.com/DumontierUntisComplex#";
+	public static final String UNITS_URI = "http://ontologies.googlecode.com/svn/trunk/src/DumontierUnitsComplex.owl";
+	public static final String DUBLIN_CORE = "http://protege.stanford.edu/plugins/owl/dc/protege-dc.owl";
+	
+}
---
+++ src/uk/ac/cam/eoc21/populatingontologies/OntologyProcessor.java
@@ -0,0 +1,61 @@
+package populatingontologies;
+
+import java.util.ArrayList;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+/**
+ * This class provides functionality to add extra information to the separate
+ * ontologies and to merge ontologies.
+ * 
+ * @author ed
+ * 
+ */
+public class OntologyProcessor {
+	public OntologyProcessor() {
+
+	}
+
+	/**
+	 * 
+	 * @param ontology
+	 *            - OntModel representation of an ontology.
+	 * @param nameSpace
+	 *            - Name space of the ontology.
+	 * @param owlClassName
+	 *            - String representation of an OWL class.
+	 * @param instanceName
+	 *            - String representation of an OWL individual.
+	 */
+	public static void addInstance(final OntModel ontology,
+			final String nameSpace, final String owlClassName,
+			final String instanceName) {
+		OntClass owlClass = ontology.getOntClass(nameSpace + owlClassName);
+		Individual anIndividual = ontology.createIndividual(nameSpace
+				+ instanceName, owlClass);
+		anIndividual.addComment("This is a test!!", instanceName);
+		System.out.println(anIndividual.getComment(instanceName));
+	}
+
+	/**
+	 * 
+	 * @param ontologies
+	 *            - ArrayList of the ontologies to merge.
+	 * @re OntModel wnOntology = ModelFactory.createOntologyModel();
+	 *     wnOntology.add(updatedOntology); wnOntology.add(techniquesOntology);
+	 *     turn OntModel of the merged ontologies.
+	 */
+	public static OntModel mergeOntologies(final ArrayList<OntModel> ontologies) {
+		OntModel mergedOntology = ModelFactory
+				.createOntologyModel(OntModelSpec.OWL_MEM);
+		for (OntModel anOntology : ontologies) {
+			mergedOntology.add(anOntology);
+		}
+		return mergedOntology;
+	}
+
+}
