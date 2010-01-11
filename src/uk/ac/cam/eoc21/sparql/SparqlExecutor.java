b025bb8f27b0fde680b74bc50fde4bb44035e54b
Ed Cannon <eoc21@cam.ac.uk>
2010-01-11 15:35:09 +0000

    SPARQL query returns all triple statements.

--- src/uk/ac/cam/eoc21/sparql/SparqlExecutor.java
+++ src/uk/ac/cam/eoc21/sparql/SparqlExecutor.java
@@ -5,8 +5,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
@@ -23,9 +26,25 @@ public class SparqlExecutor {
 	 */
 	public static void main(String[] args) throws IOException {
 		InputStream in = new FileInputStream(new File(args[0]));
-		OntModel ontologyModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
-		ontologyModel.read(in, null);
+		Model model = ModelFactory.createMemModelMaker().createModel(null);
+		model.read(in,null);
 		in.close();
+		// Create a new query
+		String queryString = 
+		//	"PREFIX foaf: <http://www.polymerinformatics.com/ChemAxiom/ChemAxiomProp.owl/> " +
+		//	"SELECT ?propertyURL " +
+		//	"WHERE {" +
+		//	"       ?foaf:hasUnit ?foaf:hasMeasurementTechnique \"DSC\" . "+
+			"SELECT *"+
+			"{ ?s ?p ?o "+
+			"      }";
+
+		com.hp.hpl.jena.query.Query query = QueryFactory.create(queryString);
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+		// Output query results	
+		ResultSetFormatter.out(System.out, results, query);
+		qe.close();
 
 	}
 
